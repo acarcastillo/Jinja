7928a07ef3c768a556af460f796341630d714e6d
Carlos Castillo Alemàn <33205655+acarcastillo@users.noreply.github.com>
2023-05-16 15:49:33 -0600

    Update test.py

--- test.py
+++ test.py
@@ -1,47 +1,51 @@
 import pytest
-import requests
-from mymodule import CourseraData
+from mymodule import format_data
 
-@pytest.fixture
-def coursera_data(monkeypatch):
-    # Mocking the response from requests.get
-    class MockResponse:
-        def __init__(self, status_code, json_data):
-            self.status_code = status_code
-            self.json_data = json_data
+def test_format_data():
+    # Test case 1: Valid row with contentIds
+    row1 = {
+        'name': 'Program 1',
+        'tagline': 'Tagline 1',
+        'id': '123',
+        'url': 'https://www.example.com/program1',
+        'contentIds': [{'contentId': 'content1'}, {'contentId': 'content2'}, {'contentId': 'content3'}]
+    }
+    expected1 = {
+        'learningProvider': 'Coursera',
+        'programName': 'Program 1',
+        'tagline': 'Tagline 1',
+        'id': '123',
+        'url': 'https://www.example.com/program1',
+        'contentIDs': 'content1;content2;content3'
+    }
+    assert format_data(row1) == expected1
 
-        def json(self):
-            return self.json_data
+    # Test case 2: Valid row without contentIds
+    row2 = {
+        'name': 'Program 2',
+        'tagline': 'Tagline 2',
+        'id': '456',
+        'url': 'https://www.example.com/program2',
+        'contentIds': []
+    }
+    expected2 = {
+        'learningProvider': 'Coursera',
+        'programName': 'Program 2',
+        'tagline': 'Tagline 2',
+        'id': '456',
+        'url': 'https://www.example.com/program2',
+        'contentIDs': ''
+    }
+    assert format_data(row2) == expected2
 
-    def mock_get_programs(start):
-        if start == '0':
-            return MockResponse(200, {
-                "paging": {
-                    "next": "1"
-                },
-                "elements": [
-                    {"id": 1, "name": "Program 1"},
-                    {"id": 2, "name": "Program 2"}
-                ]
-            })
-        elif start == '1':
-            return MockResponse(200, {
-                "paging": {},
-                "elements": [
-                    {"id": 3, "name": "Program 3"}
-                ]
-            })
-
-    monkeypatch.setattr(requests, 'get', mock_get_programs)
-
-    return CourseraData()
-
-def test_get_program_list(coursera_data):
-    # Calling the method under test
-    result = coursera_data.get_program_list()
-
-    # Assertions
-    assert len(result) == 3
-    assert result[0] == {"id": 1, "name": "Program 1"}
-    assert result[1] == {"id": 2, "name": "Program 2"}
-    assert result[2] == {"id": 3, "name": "Program 3"}
+    # Test case 3: Empty row
+    row3 = {}
+    expected3 = {
+        'learningProvider': 'Coursera',
+        'programName': None,
+        'tagline': None,
+        'id': None,
+        'url': None,
+        'contentIDs': ''
+    }
+    assert format_data(row3) == expected3
